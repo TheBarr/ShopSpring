e74c84e56ed2b3bb65152ba223a160510a6a2ab5
bartek <bartex600700@gmail.com>
2023-10-28 23:58:00 +0200

    add cart

--- src/main/java/com/thebarr/shopspring/controller/HomeController.java
+++ src/main/java/com/thebarr/shopspring/controller/HomeController.java
@@ -2,12 +2,16 @@ package com.thebarr.shopspring.controller;
 
 import com.thebarr.shopspring.model.Item;
 import com.thebarr.shopspring.repository.ItemRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -21,9 +25,24 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        List<Item> items = itemRepository.findAll();
-        model.addAttribute("items", items);
+        model.addAttribute("items", itemRepository.findAll());
         return "home";
     }
 
+    @GetMapping("/add/{itemId}")
+    public String addItemToCart(@PathVariable("itemId") Long itemId, Model model, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        if(cart == null) {
+            cart = new ArrayList<>();
+        }
+        Optional<Item> oItem = itemRepository.findById(itemId);
+        if(oItem.isPresent()) {
+            Item item = oItem.get();
+            cart.add(item);
+            session.setAttribute("cart", cart);
+        }
+        model.addAttribute("items", itemRepository.findAll());
+        return "home";
+    }
 }
